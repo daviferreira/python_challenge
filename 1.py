9f3a38a832d0445c944171107e06703799c05e91
daviferreira <contato@daviferreira.com>
2011-08-20 21:00:33 -0300

    3 is work in progress, can't figure it out.

--- 1.py
+++ 1.py
@@ -1,5 +1,3 @@
-from string import maketrans
-
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
 _in = "abcdefghijklmnopqrstuvwxyz .'()"
---
+++ 2.py
@@ -0,0 +1,22 @@
+import os
+from string import maketrans
+
+if not os.path.isfile("_2.txt"):
+	import urllib
+	sock = urllib.urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
+	source = sock.read().split("<!--")
+	sock.close()
+	source = source[2].replace("-->", "")
+	f = open("_2.txt", "w")
+	f.write(source)
+	f.close()
+else:
+	f = open("_2.txt", "r")
+	source = f.read()
+	f.close()
+
+char_count = {}
+for char in source:
+	char_count[char] = char_count.get(char, 0) + 1
+
+print char_count
