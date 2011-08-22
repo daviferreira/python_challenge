423d4ccbbc959fa4caa53bc4b2490e387048a8b2
Davi Ferreira <contato@daviferreira.com>
2011-08-22 17:52:13 -0300

    Challenge 6 solved.

--- 6.py
+++ 6.py
@@ -1,12 +1,20 @@
-import re
+import re, zipfile, sys
 
 nothing = 90052
+ids = [nothing]
+
+z = zipfile.ZipFile("channel.zip", "r")
 
 for x in range(909):
-  content = open("_6_txt/"+str(nothing)+".txt").read()
+  content = z.read(str(nothing)+".txt")
   nothing = re.sub('[^0-9]', '', content)
   try:
     int(nothing)
     print nothing
+    ids.append(nothing)
   except:
     print content
+
+
+for i in ids:
+  sys.stdout.write(z.getinfo("%s.txt" % i).comment)
